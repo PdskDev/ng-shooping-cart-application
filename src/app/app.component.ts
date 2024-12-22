f4cc2dc9bd8aa54bb4ce76c9b8b238ca8bd53c6e
NadetDev <nadetdev@gmail.com>
2024-12-22 20:47:22 +0100

    add footer

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { FooterComponent } from './layout/footer/footer.component';
 import { RouterOutlet } from '@angular/router';
 import { TopBarComponent } from './layout/top-bar/top-bar.component';
 
 @Component({
   selector: 'app-root',
-  imports: [CommonModule, RouterOutlet, TopBarComponent],
+  imports: [CommonModule, RouterOutlet, TopBarComponent, FooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
