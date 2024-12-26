ad4acc9c8cb03b255d32b64e65de98788372eec8
NadetDev <nadetdev@gmail.com>
2024-12-26 13:32:52 +0100

    add product list + product card

---
+++ src/app/data/products.data.ts
@@ -0,0 +1,76 @@
+export interface Product {
+  id: string;
+  name: string;
+  price: number;
+  thumbUrl: string;
+  imageUrl: string;
+  description: string;
+}
+
+export const PRODUCTS: Product[] = [
+  {
+    id: '8347836c-35db-4307-ac5d-cbdf19a9a50c',
+    name: 'Black+Decker Helix Performance Premium Hand Mixer, 5-Speed Mixer, Red, MX600R',
+    description:
+      'Black+Decker Helix Performance Premium Hand Mixer, 5-Speed Mixer, Red, MX600R',
+    price: 102,
+    thumbUrl: 'images/products/thumbs/8347836c-35db-4307-ac5d-cbdf19a9a50c.jpg',
+    imageUrl: 'images/products/8347836c-35db-4307-ac5d-cbdf19a9a50c.jpg',
+  },
+  {
+    id: 'f8c2cc34-ddb7-46ec-96ee-e27d1765df10',
+    name: 'KitchenAid Pro Line 5 Speed Hand Mixer, Aqua Sky',
+    description: 'KitchenAid Pro Line 5 Speed Hand Mixer, Aqua Sky',
+    price: 115,
+    thumbUrl: 'images/products/thumbs/f8c2cc34-ddb7-46ec-96ee-e27d1765df10.jpg',
+    imageUrl: 'images/products/f8c2cc34-ddb7-46ec-96ee-e27d1765df10.jpg',
+  },
+  {
+    id: '2927b969-1c97-4a36-b4ab-a0777d8690e2',
+    name: 'KitchenAid 5-Speed Ultra Power Hand Mixer',
+    description: 'itchenAid 5-Speed Ultra Power Hand Mixer',
+    price: 114,
+    thumbUrl: 'images/products/thumbs/2927b969-1c97-4a36-b4ab-a0777d8690e2.jpg',
+    imageUrl: 'images/products/2927b969-1c97-4a36-b4ab-a0777d8690e2.jpg',
+  },
+  {
+    id: '57ba7441-8bea-4340-9f49-dc369d108cf7',
+    name: 'KitchenAid 5-Speed Hand Mixer',
+    description: 'KitchenAid 5-Speed Hand Mixer',
+    price: 105,
+    thumbUrl: 'images/products/thumbs/57ba7441-8bea-4340-9f49-dc369d108cf7.jpg',
+    imageUrl: 'images/products/57ba7441-8bea-4340-9f49-dc369d108cf7.jpg',
+  },
+  {
+    id: '552809ab-7ea3-4e48-be23-3c7c53219284',
+    name: 'Cuisinart Power 5-Speed 220-Watt Hand Mixer, White',
+    description: 'Cuisinart Power 5-Speed 220-Watt Hand Mixer, White',
+    price: 119,
+    thumbUrl: 'images/products/thumbs/552809ab-7ea3-4e48-be23-3c7c53219284.jpg',
+    imageUrl: 'images/products/552809ab-7ea3-4e48-be23-3c7c53219284.jpg',
+  },
+  {
+    id: '893deaf7-5224-4702-9ba6-4fc4c52bed22',
+    name: 'KitchenAid 5 Speed Ultra Power Hand Mixer',
+    description: 'KitchenAid 5 Speed Ultra Power Hand Mixer',
+    price: 109.99,
+    thumbUrl: 'images/products/thumbs/893deaf7-5224-4702-9ba6-4fc4c52bed22.jpg',
+    imageUrl: 'images/products/893deaf7-5224-4702-9ba6-4fc4c52bed22.jpg',
+  },
+  {
+    id: '26917fd7-61c3-41d7-b7cd-3c823bae77d2',
+    name: 'KitchenAid 5-Speed Hand Mixer, Empire Red',
+    description: 'KitchenAid 5-Speed Hand Mixer, Empire Red',
+    price: 120,
+    thumbUrl: 'images/products/thumbs/26917fd7-61c3-41d7-b7cd-3c823bae77d2.jpg',
+    imageUrl: 'images/products/26917fd7-61c3-41d7-b7cd-3c823bae77d2.jpg',
+  },
+  {
+    id: '30e575f5-8997-4c34-aa44-88af74452db2',
+    name: 'Cuisinart 5-Speed Hand Mixer, Black',
+    description: 'Cuisinart 5-Speed Hand Mixer, Black',
+    price: 110,
+    thumbUrl: 'images/products/thumbs/30e575f5-8997-4c34-aa44-88af74452db2.jpg',
+    imageUrl: 'images/products/30e575f5-8997-4c34-aa44-88af74452db2.jpg',
+  },
+];
