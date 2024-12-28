078338619ef6b254dfae2057ebe6a9011a4829ee
NadetDev <nadetdev@gmail.com>
2024-12-28 21:58:43 +0100

    add stripe checkout -- todo fix compil error

--- src/app/pages/cart/cart.component.ts
+++ src/app/pages/cart/cart.component.ts
@@ -2,6 +2,9 @@ import { Component, computed } from '@angular/core';
 
 import { CartItemCardComponent } from './components/cart-item-card/cart-item-card.component';
 import { CartService } from '../../service/cart.service';
+import { HttpClient } from '@angular/common/http';
+import { environment } from '../../../environments/environment';
+import { loadStripe } from './../../../../node_modules/@stripe/stripe-js/src/index';
 
 @Component({
   selector: 'app-cart',
@@ -15,7 +18,7 @@ export class CartComponent {
   total = computed(() => this.cartService.cart().total);
   items = computed(() => this.cartService.cart().items);
 
-  constructor(private cartService: CartService) {}
+  constructor(private cartService: CartService, private http: HttpClient) {}
 
   onItemQuantityUpdate(quantity: number, id: string) {
     let increase = true;
@@ -33,5 +36,33 @@ export class CartComponent {
   onRemoveItem(id: string) {
     const existItem = this.items().find((i) => i.id === id);
     this.cartService.removeItem(existItem!);
+  }
+
+  async onCheckout() {
+    const stripe = await loadStripe(environment.STRIPE_PK);
+    const body = this.cartService.cart().items;
+    const headers = { 'Content-Type': 'application/json' };
+
+    this.http
+      .post('http://localhost:3000/api/create-checkout-session', body, {
+        headers: headers,
+      })
+      .subscribe({
+        next: async (response) => {
+          const session = response as any;
+          const result = await stripe?.redirectToCheckout({
+            sessionId: session.id,
+          });
+
+          if (result?.error) {
+            console.error(result.error);
+          }
+        },
+        error: (response) => {
+          if (response?.error) {
+            console.error(response.error);
+          }
+        },
+      });
   }
 }
---
+++ src/environments/environment.development.ts
@@ -0,0 +1,4 @@
+export const environment = {
+  STRIPE_PK:
+    'pk_test_51NssuMDCJ9zn2nguI4imbh7McYgzdALJZ7Bb0RRTIuLgBTguV5awVTNSIsC2GK7wn0vNZmdICL3yuhuYyU2pJw5g005T81hYnz',
+};
